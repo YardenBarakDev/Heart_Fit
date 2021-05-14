c27c3988656a0aa67167a00e23277c2009e43e83
Idan Koren-Israeli <39092077+idan503@users.noreply.github.com>
2021-05-14 12:26:53 +0300

    Configured Realtime Database

--- app/src/main/java/com/idan_koren_israeli/heartfit/activity/MainActivity.kt
+++ app/src/main/java/com/idan_koren_israeli/heartfit/activity/MainActivity.kt
@@ -1,18 +1,44 @@
 package com.idan_koren_israeli.heartfit.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ktx.Firebase
+import androidx.fragment.app.FragmentActivity
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
 import com.idan_koren_israeli.heartfit.R
 import com.idan_koren_israeli.heartfit.fragment.EquipmentSelectFragment
-import com.idan_koren_israeli.heartfit.model.exercise.Exercise
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var db : DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val equipmentSelect = EquipmentSelectFragment()
+        db = Firebase.database.reference
+        db.child("test_root").setValue("test_value")
 
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Get Post object and use the values to update the UI
+                val value: String? = dataSnapshot.getValue(String::class.java)
+                Log.i("pttt", value!!)
+                // ...
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w("pttt", "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        db.child("test_root").addValueEventListener(postListener)
+
+        val equipmentSelect = EquipmentSelectFragment()
         supportFragmentManager.beginTransaction().replace(R.id.mainActivity_fragment, equipmentSelect).commit()
 
     }
